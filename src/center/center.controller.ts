6bc83123f7b78e2b9507e338944da363db01a114
Raheem Kawojue <kawojue08@gmail.com>
2024-04-21 15:08:33 +0100

    feat: manage suspension, fetch staffs

--- src/center/center.controller.ts
+++ src/center/center.controller.ts
@@ -1,21 +1,22 @@
 import { Response } from 'express'
 import { Roles } from '@prisma/client'
-import { ApiTags } from '@nestjs/swagger'
 import { Role } from 'src/role.decorator'
 import { AuthGuard } from '@nestjs/passport'
 import { CenterService } from './center.service'
 import { RolesGuard } from 'src/jwt/jwt-auth.guard'
 import { FetchMedicalStaffDto } from './dto/fetch.dto'
 import { SuspendMedicalStaffDto } from './dto/auth.dto'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
 import { Controller, Get, Param, Patch, Query, Req, Res, UseGuards } from '@nestjs/common'
 
 @ApiTags('Center')
 @Controller('center')
+@ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'), RolesGuard)
 export class CenterController {
   constructor(private readonly centerService: CenterService) { }
 
-  @Get('/fetch/medical-staff')
+  @Get('/fetch/medical-staffs')
   @Role(Roles.centerAdmin)
   async fetchMedicalStaff(
     @Req() req: IRequest,
