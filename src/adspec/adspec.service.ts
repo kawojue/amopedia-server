b7f18ecffc206860ec1d8ca71c8ee6b5f3d58050
Raheem Kawojue <kawojue08@gmail.com>
2024-07-24 20:24:53 +0100

    ..

--- src/adspec/adspec.service.ts
+++ src/adspec/adspec.service.ts
@@ -29,7 +29,7 @@ export class AdspecService {
         ] as ({
             demographic: {
                 country: {
-                    contains: string;
+                    contains: string
                     mode: "insensitive"
                 }
             }
