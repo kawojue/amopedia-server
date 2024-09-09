397f822b1ec8a04cb9902ece3ce070c329e5e52f
Raheem Muyiwa <kawojue08@gmail.com>
2024-09-09 22:20:21 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      `http://localhost:5173`,
       `http://localhost:3000`,
       `http://localhost:3001`,
       `http://localhost:${PORT}`,
